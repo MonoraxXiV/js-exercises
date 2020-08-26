4ee8ecb97c405b75055fd89060eca23f9aa9086c
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-08-26 14:23:25 +0200

    Exercise 3 of date finished

--- js-complete-course/04-dates/03-age-by-select/script.js
+++ js-complete-course/04-dates/03-age-by-select/script.js
@@ -11,6 +11,42 @@
 
 (function() {
 
-    // your code here
+    document.getElementById("run").addEventListener("click", function() {
 
+        let day = document.getElementById("dob-day").value;
+        let month = document.getElementById("dob-month").value;
+        let year = document.getElementById("dob-year").value;
+
+        var today = new Date(); // i.e wed 04 may 2016 15:12:09 GMT
+
+        var todayYear = today.getFullYear();
+        var todayMonth = today.getMonth();
+        var todayDate = today.getDate();
+
+        var yearsDiff = todayYear - year;
+        var age;
+
+        if ( todayMonth < month )
+        {
+            age = yearsDiff - 1;
+        }
+        else if ( todayMonth > month )
+        {
+            age = yearsDiff ;
+        }
+
+        else //if today month = dob month
+        { if ( todayDate < day )
+        {
+            age = yearsDiff - 1;
+        }
+        else
+        {
+            age = yearsDiff;
+        }
+        }
+        alert("You are "+age+ " years old");
+
+
+    })
 })();
