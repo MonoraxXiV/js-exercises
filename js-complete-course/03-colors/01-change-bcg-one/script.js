053f18c763aabc8d67fd904bfa3eb3d63aa67658
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-08-26 09:15:07 +0200

    Finished first color exercise

--- js-complete-course/03-colors/01-change-bcg-one/script.js
+++ js-complete-course/03-colors/01-change-bcg-one/script.js
@@ -10,7 +10,23 @@
 // You will have time to focus on it later.
 
 (function() {
+    var button = document.getElementById("red");
+    button.onclick = function(){
+        document.body.style.backgroundColor = "red";
+    };
+    var button = document.getElementById("green");
+    button.onclick = function(){
+        document.body.style.backgroundColor = "green";
+    };
 
-    // your code here
+    var button = document.getElementById("blue");
+    button.onclick = function(){
+        document.body.style.backgroundColor = "blue";
+    };
+    var button = document.getElementById("yellow");
+    button.onclick = function(){
+        document.body.style.backgroundColor = "yellow";
+    };
+
 
 })();
