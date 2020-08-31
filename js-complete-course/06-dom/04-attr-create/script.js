b1ced3879193914b34fe7ee1d5cbf78aad201df4
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-08-31 11:09:34 +0200

    finished exercise 4 of dom

--- js-complete-course/06-dom/04-attr-create/script.js
+++ js-complete-course/06-dom/04-attr-create/script.js
@@ -9,8 +9,25 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+    var newImage= document.createElement("img");
+    let ImageSrc = document.getElementById("source").getAttribute("data-image");
 
-    // your code here
+    newImage.src=ImageSrc;
+    console.log(newImage.src);
+//source path finally is correct, tried a few ways, like newImage.src, but forgot to get attribute.
+    let target = document.getElementById("target").appendChild(newImage);
+    // AppendChild information:https://www.w3schools.com/jsref/met_node_appendchild.asp
+    //from my understanding changes the content of target to the newImage and it's attributes
+    //shows random kitties now so seems correct.
+
+    let element = document.getElementById("source").remove(); //removing the old source
+
+
+
+
+
+
+
 
 })();
