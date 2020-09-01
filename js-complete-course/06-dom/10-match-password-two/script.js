969a654674efcd086989818000c3cf8abaa05d5f
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-09-01 11:25:19 +0200

    finished exercise 10 of dom, when redoing def read comment

--- js-complete-course/06-dom/10-match-password-two/script.js
+++ js-complete-course/06-dom/10-match-password-two/script.js
@@ -10,7 +10,23 @@
 // You will have time to focus on it later.
 
 (function() {
+    let pass1 = document.getElementById("pass-one");
+    let pass2 = document.getElementById("pass-two");
+    document.getElementById("run").addEventListener('click', function () {
 
-    // your code here
 
+
+        if (pass1.value === pass2.value) { //compare value here, otherwise you get error from .classlist in console.
+            alert("password correct");
+            console.log ("correct");
+        } else {
+
+
+            pass1.classList.add("error");
+            pass2.classList.add("error");
+            alert("incorrect password");
+            console.log("incorrect");
+        }
+
+    })
 })();
