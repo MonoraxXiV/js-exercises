25dbdffc020c89c1b6784232dedb826467572afc
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-09-01 10:58:34 +0200

    technically works, but having trouble getting the button to work

--- js-complete-course/06-dom/09-match-password-one/script.js
+++ js-complete-course/06-dom/09-match-password-one/script.js
@@ -6,11 +6,18 @@
  * started at 26/10/2018
  */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
-(function() {
+(function () {
+    let pass1 = document.getElementById("pass-one").value;
+    let pass2 = document.getElementById("pass-two").value;
 
-    // your code here
+
+        if (pass1 === pass2) {
+//once again remember to use multiple =='s to compare 1= is an assignment
+        } else {
+            document.getElementById('pass-one').style.borderColor = "red";
+            document.getElementById('pass-two').style.borderColor = "red";
+        }
+
 
 })();
