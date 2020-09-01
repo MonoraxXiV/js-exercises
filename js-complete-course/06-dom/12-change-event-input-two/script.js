b7557e3b0c8c14fffe4d1fc6f63c9909805df54b
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-09-01 17:05:03 +0200

    Finished exercise 12 of dom

--- js-complete-course/06-dom/12-change-event-input-two/script.js
+++ js-complete-course/06-dom/12-change-event-input-two/script.js
@@ -10,7 +10,27 @@
 // You will have time to focus on it later.
 
 (function() {
+/* check how many characters more than 8 ===okay
+* Check the amount of numbers inside >2 ===okay.*/
 
-    // your code here
+var password = document.getElementById("pass-one");
+
+
+
+    function passLength(input){
+        var numberCheck = input.match(/\d/g);
+        if(input.length >= 8 && numberCheck.length>=2) {return true;}
+        else {return false;}
+
+    }
+    password.onkeyup = function() {
+
+        if (passLength(this.value)){
+            document.getElementById("validity").innerHTML ="ok";
+        }
+        else {
+            document.getElementById("validity").innerHTML =" Not ok";
+        }
+    }
 
 })();
