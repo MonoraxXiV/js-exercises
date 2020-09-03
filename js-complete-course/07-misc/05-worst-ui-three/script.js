aa11aaecf767991676e00160b5ac55406aec36a0
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-09-03 16:07:59 +0200

    finished exercise 05 of misc, def reread the comments, for the structure

--- js-complete-course/07-misc/05-worst-ui-three/script.js
+++ js-complete-course/07-misc/05-worst-ui-three/script.js
@@ -20,6 +20,9 @@
     var buttons = document.querySelectorAll("button");
     var textfields = document.querySelectorAll("input");
     var intervals=[];
+    var target= document.getElementById("target");
+    target.innerHTML="";
+
 
 
     //attempting to generate a random integer between the values
@@ -31,12 +34,34 @@
         intervals.push(interval);
     })
     function slotsMachine(target) {
-
-        var number = Math.floor(Math.random() * (parseInt(target.dataset.max) -parseInt(target.dataset.min))) + parseInt(target.dataset.min);
         //random number generated between the min-max of textfields.
+        var number = Math.floor(Math.random() * (parseInt(target.dataset.max) -parseInt(target.dataset.min))) + parseInt(target.dataset.min);
+
         target.value=number;
+        //adding 0 to numbers lower to 10.
+        if (number <10){
+            target.value="0"+number;
+        }
 
     }
+    buttons.forEach((button,index)=>{
+        //Create spans inside your function, cause you need them here.
+        var span =document.createElement("SPAN");
+        //adding class due to having another span in html, you want the ones you made, not ones that are already there.
+        span.classList.add("telephone");
+        //Set to min as starting value, looks cleaner otherwise it starts with random values.
+        span.innerHTML=textfields[index].dataset.min;
+        target.appendChild(span);
 
+        button.addEventListener("click",function(){
+            //stops interval when you press the button, used an array to keep index, as to not stop all intervals at once.
+            clearInterval(intervals[index]);
+            //calls upon the values of span, who share the class tag telephone.
+            var spans= document.getElementsByClassName("telephone");
+            //assigning values to the correct span.
+            spans[index].innerHTML=textfields[index].value;
+
+        })
+    })
 
 })();
