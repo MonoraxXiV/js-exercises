1e80184360b23b53625709504077c541591bfb2b
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-09-03 11:57:39 +0200

    finished exercise 3 of misc learned valueAsNumber, onchange

--- js-complete-course/07-misc/03-worst-ui-one/script.js
+++ js-complete-course/07-misc/03-worst-ui-one/script.js
@@ -9,8 +9,26 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    // your code here
+    /*
+    An exercise in creating the worst possible user interface for dialing a phone number:
+
+
+     use the value of the slider to show the phone number (with a leading 0) in the target paragraph
+     (target)
+
+      (from 0460000000 to 0499999999).
+     */
+
+    var input= document.getElementById("slider")
+
+    input.addEventListener("change",getValue)
+
+    function getValue(e){
+        var value= e.target.valueAsNumber;
+        console.log(value);
+        document.getElementById("target").innerHTML="0"+value;
+    }
 
 })();
