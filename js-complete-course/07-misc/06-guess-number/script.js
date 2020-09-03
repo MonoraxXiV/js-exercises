3eacd57e1c6a31802ae80d7413cb75ffd06d835d
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-09-03 17:01:06 +0200

    Added attempt to structure in comments, added conditions

--- js-complete-course/07-misc/06-guess-number/script.js
+++ js-complete-course/07-misc/06-guess-number/script.js
@@ -11,6 +11,29 @@
 
 (function() {
 
-    // your code here
+ // 1 page get loaded, random number between 0 and 100 is generated.
+ // 2. prompts user for a number.
+ // 3.compares guess with number -> responds higher or lower or that's it if correct.
+ // 4. add counter for Y, where Y is the amount of guesses.
+ //5. When number is guessed show how many guesses it took.
+    var isCorrect = new Boolean(true);
+    var randomNumber=Math.floor(Math.random() * 100) + 1; // returns a random integer from 1 to 100
+    console.log(randomNumber) //for now keep for debug reasons, remove later.
 
+    var guess = prompt("Please enter a number");
+    while (isCorrect != true)
+
+    if (guess > randomNumber){
+        alert("lower");
+        return isCorrect=false
+
+    }
+    else if(guess < randomNumber){
+        alert("higher");
+        return isCorrect=false
+    }
+    else if (guess == randomNumber){
+        alert("that's it")
+        return isCorrect=true;
+    }
 })();
