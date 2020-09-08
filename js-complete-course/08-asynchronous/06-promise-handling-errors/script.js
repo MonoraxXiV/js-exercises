4207950d515c3a63f374003f00322e9bab5f3b56
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-09-08 12:10:19 +0200

    finished 08-06, added message for rejectHandler

--- js-complete-course/08-asynchronous/06-promise-handling-errors/script.js
+++ js-complete-course/08-asynchronous/06-promise-handling-errors/script.js
@@ -10,5 +10,26 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    let run= document.getElementById("run")
+
+    run.addEventListener("click", function() {
+
+
+
+        function resolveHandler(resolve){
+            console.log("success");
+            resolve.forEach(resolve => {
+                console.log(resolve)
+            })
+        }
+
+        function rejectHandler(reject){
+            console.error("You had a 50% chance and rolled badly");
+        }
+
+        //getPosts need parameters reject and resolve.
+        //As the promise was already made in promises.js, so no new one was needed.
+        window.lib.getPersons().then(resolveHandler,rejectHandler);
+
+    })
 })();
