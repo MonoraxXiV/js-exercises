61b1dc4a4f8e0fdb737569a3065c098b6af01e84
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-09-08 16:11:48 +0200

    attempting to obtain data using xhttp

--- js-complete-course/09-fetch/01-list-to-console/script.js
+++ js-complete-course/09-fetch/01-list-to-console/script.js
@@ -6,9 +6,22 @@
  * started at 12/05/2019
  */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
+//use fetch to find the data
+//use REST architecture to retrieve the data.
+//then use REST to show the data
+//display data in console log.
 
 (() => {
-    // your code here
+   let run = document.getElementById("run")
+
+   run.addEventListener("click", function () {
+      fetch('http://localhost:3000/heroes');
+      var xhttp = new XMLHttpRequest();
+      xhttp.open("GET", 'http://localhost:3000/heroes', true);
+      xhttp.send();
+
+      let response= xhttp.responseText;
+      console.log(response);
+   })
+
 })();
