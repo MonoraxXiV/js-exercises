5c5b0b8fcb8c87b3ab5eadd427c203e427f67534
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-09-08 14:43:31 +0200

    08-08 finished, watch out for flow of functions

--- js-complete-course/08-asynchronous/08-async-get-comments/script.js
+++ js-complete-course/08-asynchronous/08-async-get-comments/script.js
@@ -10,5 +10,29 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    let run = document.getElementById("run")
+
+    run.addEventListener("click", function () {
+        let array;
+        async function resolveHandler(resolve){
+
+            await resolve.forEach(post => {
+                window.lib.getComments(post.id).then(resolve2Handler,rejectHandler);
+                array=post;
+            })
+        }
+        //this one we do not await on as he is already part of the await in resolve.
+      function resolve2Handler(comments){
+           array.comment=comments;
+             console.log(array);
+        }
+
+       async function rejectHandler(){
+            await console.error("error");
+        }
+
+        window.lib.getPosts().then(resolveHandler,rejectHandler);
+
+
+    })
 })();
