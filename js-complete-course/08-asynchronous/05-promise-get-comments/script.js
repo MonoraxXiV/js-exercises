98fd5e7e30bf77deb78a7a7313a9970c0da7bad7
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-09-08 11:24:22 +0200

    08-05, getPosts work, getComments return error

--- js-complete-course/08-asynchronous/05-promise-get-comments/script.js
+++ js-complete-course/08-asynchronous/05-promise-get-comments/script.js
@@ -10,5 +10,28 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    //Like in 04, promise is included in the functions you call from promises.js
+    //getting the posts are the same as in 04, you might need to loop inside them again
+    let run = document.getElementById("run")
+
+    run.addEventListener("click", function () {
+
+        function resolveHandler(resolve){
+            console.log("success");
+            resolve.forEach(resolve => {
+                window.lib.getComments().then(rejectHandler,rejectHandler);
+                console.log(resolve);
+            })
+        }
+
+        function rejectHandler(){
+            console.error("error");
+        }
+
+        //getPosts need parameters reject and resolve.
+        //As the promise was already made in promises.js, so no new one was needed.
+        window.lib.getPosts().then(resolveHandler,rejectHandler);
+
+
+    })
 })();
