553e09e1fecb76816bede9b2f58e959b61a9c55b
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-09-08 11:09:39 +0200

    Finished exercise 4 of asynchronous

--- js-complete-course/08-asynchronous/04-promise-get-posts/script.js
+++ js-complete-course/08-asynchronous/04-promise-get-posts/script.js
@@ -9,6 +9,36 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+//difference between 01 and 04 is in the html 01 calls from callbacks.
+// 04 calls from promises.
 (() => {
-    // your code here
+
+   let run= document.getElementById("run")
+
+        run.addEventListener("click", function() {
+
+
+
+        function resolveHandler(resolve){
+            console.log("success");
+            resolve.forEach(resolve => {
+                console.log(resolve)
+            })
+        }
+
+        function rejectHandler(reject){
+            console.error("error");
+        }
+
+        //getPosts need parameters reject and resolve.
+        //As the promise was already made in promises.js, so no new one was needed.
+        window.lib.getPosts().then(resolveHandler,rejectHandler);
+
+    })
+
+
+
+
+
+
 })();
