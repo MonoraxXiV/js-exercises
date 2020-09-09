5e0207a2ff308bd72687a663089425f80f099c7f
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-09-09 12:31:52 +0200

    push before lunch, trying to show the queryselector content to debug a bit

--- js-complete-course/09-fetch/02-list-to-template/script.js
+++ js-complete-course/09-fetch/02-list-to-template/script.js
@@ -15,18 +15,28 @@
     // cloneNode, see https://blog.teamtreehouse.com/creating-reusable-markup-with-the-html-template-element under html templates.
     let run = document.getElementById("run")
 
+
+
     run.addEventListener("click", function () {
         //fetch was correct
         fetch('http://localhost:3000/heroes')
             .then (resolve=> resolve.json())
-            .then(data=> document.getElementById("target").innerHTML =data)
+            .then(xmenAmount=> showAllData())
 
 
-        function getTemplate (){
-            var tmpl = document.getElementById('tpl-hero');
-            document.body.appendChild(tmpl.content.cloneNode(true));
-            console.log(tmpl);
+        //assuming a loop will be involved to put all the x-men into a template.
+        function showAllData(xmenAmount) {
+            //currently using 5 instead of something.length
+            for (let i = 0; i < 5; i++) {
+                //looping for each of the xmen
+                let listOfXmen= xmenAmount[i];
+                //when using document we can call upon the queryselector to specify what we seek.
+                let xmen = document.getElementById('tpl-hero').content.cloneNode(true);
+                //selector apparently wants a dot, otherwise it complains
+                xmen.querySelector('.name').innerText=listOfXmen.name;
 
+
+            }
         }
 
     })
